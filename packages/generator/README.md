c970ba329f8ee0a22ac383e0778c26862276aa96
Carlos Sosa <cesosag@gmail.com>
2021-03-18 13:51:34 +0000

    Add generator package

---
+++ packages/generator/README.md
@@ -0,0 +1,13 @@
+# Generate
+
+This package allows to easily create components for React projects that use `styled-components` library.
+
+## Usage
+
+1. Run `yarn add -D @csjs/generate`
+
+2. Add `"generate": "generate"` to the scripts in `package.json`.
+
+3. Run `yarn generate` and follow the propmt questions.
+
+You can make sure this module lints with itself using `yarn lint`.
---
+++ packages/generator/plopfile.js
@@ -0,0 +1,41 @@
+module.exports = (plop) => {
+	plop.setGenerator('component', {
+		description: 'Create a component',
+		prompts: [
+			{
+				type: 'input',
+				name: 'name',
+				message: 'What is the component name?',
+			},
+		],
+		actions: [
+			{
+				type: 'add',
+				path: 'components/{{pascalCase name}}/{{pascalCase name}}.jsx',
+				templateFile: 'templates/Component.jsx.hbs',
+			},
+			{
+				type: 'add',
+				path: 'components/{{pascalCase name}}/{{pascalCase name}}.styles.js',
+				templateFile: 'templates/Component.styles.js.hbs',
+			},
+			{
+				type: 'add',
+				path: 'components/{{pascalCase name}}/index.js',
+				templateFile: 'templates/index.js.hbs',
+			},
+			{
+				type: 'add',
+				path: 'components/index.js',
+				templateFile: 'templates/components-index.js.hbs',
+				skipIfExists: true,
+			},
+			{
+				type: 'append',
+				path: 'components/index.js',
+				pattern: '/* INJECT_EXPORT */',
+				template: 'export { default as {{ properCase name }} } from \'./{{ properCase name }}\'',
+			},
+		],
+	})
+}
