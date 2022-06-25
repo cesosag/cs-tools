41feba4081f9376b6b049d214f7278a3dec3297d
Carlos Sosa <cesosag@gmail.com>
2022-06-25 18:24:35 +0100

    Add TypeScript support for the generator

--- packages/generator/README.md
+++ packages/generator/README.md
@@ -1,6 +1,6 @@
 # Generate
 
-This package allows to easily create components for React projects that use `styled-components` library.
+This package allows to easily create components for React projects (with either JavaScript or TypeScript) that use `styled-components` library.
 
 ## Usage
 
@@ -8,6 +8,6 @@ This package allows to easily create components for React projects that use `sty
 
 2. Add `"generate": "generate"` to the scripts in `package.json`.
 
-3. Run `yarn generate` and follow the propmt questions.
+3. Run `yarn generate` and follow the prompt questions.
 
 You can make sure this module lints with itself using `yarn lint`.
--- packages/generator/plopfile.js
+++ packages/generator/plopfile.js
@@ -1,41 +1,102 @@
+const prompts = [
+	{
+		type: 'input',
+		name: 'name',
+		message: 'What is the component name?',
+	},
+	{
+		type: 'list',
+		name: 'tsOrJs',
+		message: 'Select type of generated files',
+		choices: ['TypeScript', 'JavaScript'],
+		default: 0,
+	},
+]
+
+const jsActions = [
+	{
+		type: 'add',
+		path: 'components/{{pascalCase name}}/{{pascalCase name}}.jsx',
+		templateFile: 'templates/js/Component.jsx.hbs',
+	},
+	{
+		type: 'add',
+		path: 'components/{{pascalCase name}}/{{pascalCase name}}.styles.js',
+		templateFile: 'templates/js/Component.styles.js.hbs',
+	},
+	{
+		type: 'add',
+		path: 'components/{{pascalCase name}}/index.js',
+		templateFile: 'templates/js/index.js.hbs',
+	},
+	{
+		type: 'add',
+		path: 'components/{{pascalCase name}}/data.mock.js',
+		templateFile: 'templates/js/data.mock.js.hbs',
+	},
+	{
+		type: 'add',
+		path: 'components/{{pascalCase name}}/{{pascalCase name}}.test.jsx',
+		templateFile: 'templates/js/Component.test.jsx.hbs',
+	},
+	{
+		type: 'add',
+		path: 'components/index.js',
+		templateFile: 'templates/js/components-index.js.hbs',
+		skipIfExists: true,
+	},
+	{
+		type: 'append',
+		path: 'components/index.js',
+		pattern: '/* INJECT_EXPORT */',
+		templateFile: 'templates/js/index.js.hbs',
+	},
+];
+
+const tsActions = [
+	{
+		type: 'add',
+		path: 'components/{{pascalCase name}}/{{pascalCase name}}.tsx',
+		templateFile: 'templates/ts/Component.tsx.hbs',
+	},
+	{
+		type: 'add',
+		path: 'components/{{pascalCase name}}/{{pascalCase name}}.styles.ts',
+		templateFile: 'templates/ts/Component.styles.ts.hbs',
+	},
+	{
+		type: 'add',
+		path: 'components/{{pascalCase name}}/index.ts',
+		templateFile: 'templates/ts/index.ts.hbs',
+	},
+	{
+		type: 'add',
+		path: 'components/{{pascalCase name}}/data.mock.ts',
+		templateFile: 'templates/ts/data.mock.ts.hbs',
+	},
+	{
+		type: 'add',
+		path: 'components/{{pascalCase name}}/{{pascalCase name}}.test.tsx',
+		templateFile: 'templates/ts/Component.test.tsx.hbs',
+	},
+	{
+		type: 'add',
+		path: 'components/index.ts',
+		templateFile: 'templates/ts/components-index.ts.hbs',
+		skipIfExists: true,
+	},
+	{
+		type: 'append',
+		path: 'components/index.ts',
+		pattern: '/* INJECT_EXPORT */',
+		templateFile: 'templates/ts/index.ts.hbs',
+	},
+];
+
 export default (plop) => {
 	plop.setGenerator('component', {
 		description: 'Create a component',
-		prompts: [
-			{
-				type: 'input',
-				name: 'name',
-				message: 'What is the component name?',
-			},
-		],
-		actions: [
-			{
-				type: 'add',
-				path: 'components/{{pascalCase name}}/{{pascalCase name}}.jsx',
-				templateFile: 'templates/Component.jsx.hbs',
-			},
-			{
-				type: 'add',
-				path: 'components/{{pascalCase name}}/{{pascalCase name}}.styles.js',
-				templateFile: 'templates/Component.styles.js.hbs',
-			},
-			{
-				type: 'add',
-				path: 'components/{{pascalCase name}}/index.js',
-				templateFile: 'templates/index.js.hbs',
-			},
-			{
-				type: 'add',
-				path: 'components/index.js',
-				templateFile: 'templates/components-index.js.hbs',
-				skipIfExists: true,
-			},
-			{
-				type: 'append',
-				path: 'components/index.js',
-				pattern: '/* INJECT_EXPORT */',
-				template: 'export { default as {{ properCase name }} } from \'./{{ properCase name }}\'',
-			},
-		],
-	})
+		prompts,
+		actions: ({ tsOrJs }) => tsOrJs === 'TypeScript' ? tsActions : jsActions,
+	});
 }
